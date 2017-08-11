9b3bf6d3c8197dd13bd33a9d4bc0f705a9103e3a
SpaceHotDog <nicolassolignac@gmail.com>
2017-08-11 14:15:06 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,15 +1,19 @@
 # manage.py
 
 import os
-from flask_script import Manager # class for handling a set of commands
+from flask_script import Manager # Class for handling a set of commands.
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
+# Import the models so that the script can find the models to be migrated.
 from app import models
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
+# The MigrateCommand contains a set of migration commands.
 migrate = Migrate(app, db)
-manager = Manager(app)
+# The Manager class keeps track of all the commands and handles how they are called from the command line.
+manager = Manager(app) 
 
+# Manager also adds the migration commands and enforces that they start with db.
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
