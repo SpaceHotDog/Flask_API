a11c4b7c4f546156d5d70c9799d148b99e932e2c
SpaceHotDog <nicolassolignac@gmail.com>
2017-08-11 12:18:29 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,11 @@ def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    
+# We've also disabled track modifications for SQLAlchemy because 
+# it'll be deprecated in future due to it's significant performance overhead. 
+# For debugging enthusiasts, you can set it to True for now.
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
