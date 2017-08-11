7bc20eefe40bb6db38d4f9133a62d4711454cb74
SpaceHotDog <nicolassolignac@gmail.com>
2017-08-11 15:42:16 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort
 
 # local import
 from instance.config import app_config
@@ -10,6 +11,42 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
+    from api.models import Bucketlist
+
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    return app
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
